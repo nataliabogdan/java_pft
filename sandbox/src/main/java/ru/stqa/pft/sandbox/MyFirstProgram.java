6004e595b958f5eab8c6434404517a12a07029eb
Natalia Bogdan <plopik.tala@gmail.com>
2016-11-20 21:15:33 +0300

    Created package for my first program

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,12 +1,12 @@
+package ru.stqa.pft.sandbox;
+
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, world");
+		System.out.println("Hi, Natalia!");
 		
 	}
-
-
 }
 
 
