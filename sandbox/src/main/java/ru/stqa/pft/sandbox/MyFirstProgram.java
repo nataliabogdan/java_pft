4b6b84fdb5e7dda9cc4c2c4306a3271412dc992d
Natalia Bogdan <plopik.tala@gmail.com>
2016-11-20 21:33:16 +0300

    Changed format of Class

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,11 +2,10 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
+    System.out.println("Hi, Natalia!");
+  }
 
-		System.out.println("Hi, Natalia!");
-		
-	}
 }
 
 
