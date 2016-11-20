e4a88864b7d2500c345393a5e7f53cf4048d7c58
Natalia Bogdan <plopik.tala@gmail.com>
2016-11-20 23:28:05 +0300

    Added some functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,9 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hi, Natalia!");
+    hello("world");
+    hello("Natalia");
+    double l = 5;
+    double a = 3;
+    double b = 2;
+    System.out.println("Area of square with side " + l + " = " + area(l));
+    System.out.println("Area of a rectangle with sides " + a + " and " + b + " = " + area(a, b));
+
   }
 
+  public static void hello(String somebody){
+    System.out.println( "Hello, " + somebody + "! ");
+  }
+
+  public static double area(double len){
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+
+  }
 }
+
 
 
