13bb38df00b8833a89706387729ee0e317a087cb
Natalia Bogdan <plopik.tala@gmail.com>
2016-11-22 00:00:00 +0300

    Added 4 tack

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -12,5 +12,9 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+  public double distance(Point p){
+    return Math.sqrt(Math.pow((p.x - x),2) + Math.pow((p.y - y),2));
+
+  }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/PrintDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PrintDistance.java
@@ -7,17 +7,11 @@ public class PrintDistance {
 
   public static void main(String[] args){
 
-    Point p1 = new Point(10,5);
+    Point p1 = new Point(10,4);
     Point p2 = new Point(8,1);
 
 
-    System.out.println("Distance between 2 points: " + "x1 = " + p1.x + " y1 = " + p1.y + " and "  + " x2 = " + p2.x + " y2 = " + p2.y + " is equally to " + distance(p1,p2));
-  }
-
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
-
+    System.out.println("Distance between 2 points is equally to " + p1.distance(p2));
   }
 
 
