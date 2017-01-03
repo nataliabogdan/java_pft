746404fb9209d0a41f0323f67e500fadd5ad9257
Natalia Bogdan <plopik.tala@gmail.com>
2017-01-03 23:01:30 +0300

    Enhancement of code for Group Tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class ContactDeletionTests extends TestBase {
 
-  @Test
+  @Test (enabled = false)
   public void contactDeletion(){
     app.getNavigationHelper().gotoHomePage();
     if (!app.getContactHelper().isThereAContact()) {
